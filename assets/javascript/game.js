b862bf92f523c29fedb625e1bc420adfd82dfc12
Retr0 <Ghozt24@gmail.com>
2018-09-15 19:37:28 -0700

    version 1.2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,8 @@ var wins = 0;
 var losses = 0;
 
 function start(){
+
+    $("#winCount").html("Wins; " + wins);
     numMatch = Math.floor(Math.random() * 81 + 1);
     
     $("#randomizedNum").html("Number to match: " + numMatch);
@@ -28,27 +30,54 @@ function start(){
 function numInput(){
 
     $("#crystyle1").on("click", function(){
+        $("#crystyle1").addClass("animated bounce");
+        var timer = setInterval(function(){
+            $("#crystyle1").removeClass("animated bounce");
+            clearInterval(timer);
+        }, 1000);
+
         userNum = userNum + blueNum;
         
         $("#currentScore").html("Current score: " + userNum);
 
         winCondition();
     });
+
     $("#crystyle2").on("click", function(){
+        $("#crystyle2").addClass("animated bounce");
+        var timer = setInterval(function(){
+            $("#crystyle2").removeClass("animated bounce");
+            clearInterval(timer);
+        }, 1000);
+
         userNum = userNum + yellowNum;
         
         $("#currentScore").html("Current score: " + userNum);
 
         winCondition();
     });
+
     $("#crystyle3").on("click", function(){
+        $("#crystyle3").addClass("animated bounce");
+        var timer = setInterval(function(){
+            $("#crystyle3").removeClass("animated bounce");
+            clearInterval(timer);
+        }, 1000);
+
         userNum = userNum + greenNum;
         
         $("#currentScore").html("Current score: " + userNum);
 
         winCondition();
     });
+
     $("#crystyle4").on("click", function(){
+        $("#crystyle4").addClass("animated bounce");
+        var timer = setInterval(function(){
+            $("#crystyle4").removeClass("animated bounce");
+            clearInterval(timer);
+        }, 1000);
+        
         userNum = userNum + redNum;
         
         $("#currentScore").html("Current score: " + userNum);
@@ -60,18 +89,48 @@ function numInput(){
 function winCondition(){
     if (userNum === numMatch){
         wins++;
-        alert("You win!!!");
-        $("#winCount").html("Wins; " + wins);
+        $("#alert").html("YOU WON!");
+        
+        var timer = setInterval(function(){
+            $("#alert").html("");
+            clearInterval(timer);
+        }, 2000);
+
+        
+        $("#winCount").html("Wins: " + wins);
         start();
     }
     else if (userNum > numMatch){
         losses++;
-        alert("You lose!!!");
-        $("#lossCount").html("Losses; " + losses);
+        $("#alert").html("YOU LOST!");
+       
+        var timer = setInterval(function(){
+            $("#alert").html("");
+            clearInterval(timer);
+        }, 2000);
+        
+     
+        $("#lossCount").html("Losses: " + losses);
         start();
     }
 }
 
+function resetGame(){
+    $("#reset").on("click", function(){
+        wins = 0;
+        losses = 0;
+        userNum = 0;
+        
+        $("#winCount").html("Wins: " + wins);
+        $("#lossCount").html("Losses: " + losses);
+        $("#currentScore").html("Current score: " + userNum);
+        
+        start();
 
+    })
+}
+
+
+resetGame();
 numInput();
 start();
